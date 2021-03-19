8a84909e3d4bd7913437649591895b04dd9010c6
Jackson Riley <jack.riley801@gmail.com>
2021-03-19 15:06:59 -0600

    fixed stuff

--- poorly_coded_store/models.py
+++ poorly_coded_store/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.fields.related import ManyToManyField
 
 class Product(models.Model):
     description = models.CharField(max_length=45)
--- poorly_coded_store/views.py
+++ poorly_coded_store/views.py
@@ -1,16 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Order, Product
 
 def index(request):
+    if not "total" in request.session:
+        request.session['total'] = 0
     context = {
         "all_products": Product.objects.all()
     }
     return render(request, "store/index.html", context)
 
 def checkout(request):
+    context = Product.objects.get(id = request.POST["id"])
     quantity_from_form = int(request.POST["quantity"])
-    price_from_form = float(request.POST["price"])
-    total_charge = quantity_from_form * price_from_form
+    price_from_form = float(context.price)
+    total_charge = round(quantity_from_form * price_from_form, 2)
     print("Charging credit card...")
     Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge)
-    return render(request, "store/checkout.html")
+    request.session['total'] += round(total_charge,2)
+    return redirect("/thanks/")
+    
+def thanks(request):
+    pass
+    context = { "current_order" : Order.objects.last()   
+     }
+    return render (request, "store/checkout.html", context)
